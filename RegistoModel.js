9ea35c6dd6bbbe5aba81297d6f11435ff5415a3b
JElmanoAF <joao515515@hotmail.com>
2021-04-12 01:05:34 +0100

    Criação do ficheiro RegistoModel.js

---
+++ RegistoModel.js
@@ -0,0 +1,24 @@
+var mongoose = require ('mongoose');
+
+// schema
+var covidSchema = mongoose.Schema ({
+    date: {
+        type: String,
+        required: true
+    },
+    confirmed_new: {
+        type: Number,
+        required: true
+    },
+    internados_uci: {
+        type: Number,
+        required: true
+    }
+});
+
+// Export Covid Model
+var Covid = module.exports = mongoose.model ('covid',covidSchema);
+
+module.exports.get = function (callback, limit) {
+   Covid.find (callback) .limit (limit);
+}
